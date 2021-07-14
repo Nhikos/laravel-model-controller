1b5a5b375b5d41d927fcf3f528014c9ba0c97c56
Nhikos <4ndre.sergi@gmail.com>
2021-07-14 15:40:48 +0200

    ex complete

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,19 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset("css/style.css") }}">
     <title>Document</title>
 </head>
 <body>
-    <h1>Ciao</h1>
-
-    @foreach ($movies as $movie)
-        <div>
+    <div class="container">
+        @foreach ($movies as $movie)
+        <div class="card">
             <h2>{{ $movie -> title }}</h2>
-            <h4>{{ $movie -> original_title }}</h2>
-
-            <hr>
-
+            <h3>{{ $movie -> original_title }}</h2>
+            <h4>{{ $movie -> nationality }}</h2>
+            <h4>{{ $movie -> date }}</h2>
+            <h5>{{ $movie -> vote }}</h2>
         </div>
-    @endforeach
+        @endforeach
+    </div>
 </body>
 </html>
